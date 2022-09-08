806635c7f65e6b923d2ecd469d67105b820c46fc
Moriviano <dpromise24@gmail.com>
2022-09-08 15:25:01 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print the string in the function
+*
+* Description: using the main function
+*this program prints *programing is like building a multilingual puzzle
+* Return: 0
+*/
+int main(vold)
+{ puts(*\*programming is like building a multilingual puzzle*);
+return (0);
