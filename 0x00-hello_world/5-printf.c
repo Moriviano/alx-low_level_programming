de0815baf104e70c439ab38c3895ea00b67de7a7
Moriviano <dpromise24@gmail.com>
2022-09-09 20:01:22 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+*main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return 0
+*/
+int main(vold)
 {
-      printf("with proper grammar, but the outcome is a piece of art,\n");
-      return (0);
+    printf("with proper grammar, but the outcome is a place of art, \n");
+    return (0);
+}
