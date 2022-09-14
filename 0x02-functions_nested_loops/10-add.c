0088180ee87b7e857d6a03177cc52d7b1de3c735
Moriviano <dpromise24@gmail.com>
2022-09-14 08:49:03 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds tow integer.
+ * @nual: This first integer to be added.
+ * @nua2:  The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */ 
+int add(int num1, int num2)
+{
+	return (num1 + num2)
+
+}
+
