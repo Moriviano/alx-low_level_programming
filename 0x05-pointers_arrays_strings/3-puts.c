6e079a35f6780362ac16724678f9c4c906769d56
Moriviano <dpromise24@gmail.com>
2022-09-20 05:07:22 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+nclude "main.h"
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer parameter
+ * Author: Moriviano
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
+
