b3165045cd8daf7fec66ec2fa353001328569dad
Moriviano <dpromise24@gmail.com>
2022-09-22 07:14:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,16 @@ int main(void)
 	for (a = 48; a <=57; a++)
 	{
 		for (b = 49; b <= 57; b++)
-		{	
+		{
 			if ( a != b && b > a)
-			{	
+			{
 				putchar(a);
 				putchar(b);
 				putchar(',');
 			}
 		}
 	}
+	putchar('\n');
+	return (0);
 }
-
 
