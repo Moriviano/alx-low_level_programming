dcfd44ddf729b9b20a358eaad6cf9bfe3e6980a1
Moriviano <dpromise24@gmail.com>
2022-09-22 06:43:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * autor: Moriviano
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sammy;
+	char kingx;
+
+	for (sammy = 48; sammy < 58; sammy++)
+		putchar(sammy);
+	for (kingx = 'a'; kingx <= 'f'; kingx++)
+		putchar(kingx);
+	putchar('\n');
+	return (0);
+}
+
