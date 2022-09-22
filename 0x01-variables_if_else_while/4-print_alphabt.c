18cb46fa982035cf76385bbcad3d699dcc52e51c
Moriviano <dpromise24@gmail.com>
2022-09-22 06:08:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Moriviano
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char sammy;
+
+	for (sammy = 'a'; sammy <= 'z'; sammy++)
+	{
+		if (sammy != 'q' && sammy != 'e')
+			putchar(sammy);
+	}
+	putchar('\n');
+	return (0);
+}
+
