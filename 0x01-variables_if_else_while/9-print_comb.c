f5a33439a893e45cf697feb7c5a2a54bd39cd49a
Moriviano <dpromise24@gmail.com>
2022-09-22 06:53:23 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: Moriviano
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sammy;
+
+	for (sammy = 48; sammy < 58; sammy++)
+	{
+		putchar(sammy);
+		if (sammy != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
