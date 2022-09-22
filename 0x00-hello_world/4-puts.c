fe5eddc8913b891fc377ba05058ed91f8b606f31
Moriviano <dpromise24@gmail.com>
2022-09-22 01:26:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,13 +9,6 @@
  */
 int main(void)
 {
-	char string[70] = "\"Programming is like building a multilingual puzzle\"";
-
-	puts(string);
-
-	/* Tried printing it directly */
-
-	puts("\"Programming is like building a multilingual puzzle\"");
-	return (0);
+char string[70] = "\"Programming is like building a multilingual puzzle\"";
+puts(string);
 }
-
